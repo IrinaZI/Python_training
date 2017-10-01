5cae298f10295cd989f119a5092bb14c2925f097
IrinaZI <30446195+IrinaZI@users.noreply.github.com>
2017-10-01 19:10:01 +0400

    Login и Logaut вынесены в фикстуру

--- Test/test_add_group.py
+++ Test/test_add_group.py
@@ -3,12 +3,10 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.Login(username="admin", password="secret")
     app.group.create(Group(name="frfrfr", header="frfrfr", footer="frfrfr"))
-    app.session.Logout()
+
 
 def test_add_empty_group(app):
-    app.session.Login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.Logout()
+
 

489358e49f68dde8752c76b5d39d8ae6cc9c75e9
IrinaZI <30446195+IrinaZI@users.noreply.github.com>
2017-10-01 23:58:35 +0400

    Добавлены предусловия для наличия хотябы одного элемнта для тестов модификации группы и удаления контакта

--- Test/test_del_contact.py
+++ Test/test_del_contact.py
@@ -1,5 +1,10 @@
-
+from model.contact import Contact
 
 def test_delete_first_contact(app):
-
+    if app.contact.count() == 0:
+        app.contact.create(Contact(Firstname="Test", Middlename="", Lastname="", Nickname="", Title="",
+                       Company="", Address="", Home="", Mobile="", Work="",
+                       Fax="", Email="", Email2="", Email3="", Homepage="",
+                       Bday="", Bmonth="", Byear="", Aday="", Amonth="", Ayear="",
+                       Address2="", Phone2="", Notes=""))
     app.contact.delete_first_contact()

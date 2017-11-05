c3c0db67f763c6f31e0accdfb96c413432e9d0cc
IrinaZI <30446195+IrinaZI@users.noreply.github.com>
2017-11-05 20:48:22 +0300

    Скорректирована инициализация соединения с базой данных

--- fixture/orm.py
+++ fixture/orm.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from model.group import Group
 from model.contact import Contact
 from pymysql.converters import decoders
+from pymysql.converters import encoders, decoders, convert_mysql_timestamp
 
 class ORMFixture:
 
@@ -13,17 +14,22 @@ class ORMFixture:
         id = PrimaryKey(int, column='group_id')
         name = Optional(str, column='group_name')
         header = Optional(str, column='group_header')
-        footer = Optional(str, column='group_Footer')
+        footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups" , column="id", reverse="groups", lazy=True)
 
     class ORMContact(db.Entity):
         _table_='addressbook'
         id = PrimaryKey(int, column='id')
-        firstname = Optional(str,column='firstname')
+        firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table='address_in_groups', column='group_id', reverse='contacts', lazy=True)
 
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql',host=host, database = name, user = user, password = password, conv=decoders )
+        conv = encoders
+        conv.update(decoders)
+        conv[datetime] = convert_mysql_timestamp
+        self.db.bind('mysql',host=host, database = name, user = user, password = password, conv=conv )
         self.db.generate_mapping()
         sql_debug(True)
 
@@ -34,7 +40,7 @@ class ORMFixture:
         return list(map(convert, groups))
 
 
-    def convert_contacts_to_model(self,contacts):
+    def convert_contacts_to_model(self, contacts):
         def convert(contact):
             return Contact(id=str(contact.id), Firstname = contact.firstname, Lastname = contact.lastname)
         return list(map(convert, contacts))
@@ -47,3 +53,8 @@ class ORMFixture:
     @db_session
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)

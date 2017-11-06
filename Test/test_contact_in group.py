e00c022d7ade9fedbf275cebad033c4e92265d61
IrinaZI <30446195+IrinaZI@users.noreply.github.com>
2017-11-06 16:47:38 +0300

    Проверка добавления контакта в группу

--- Test/test_contact_in group.py
+++ Test/test_contact_in group.py
@@ -1,16 +1,36 @@
 from model.group import Group
+from model.contact import Contact
+from fixture.orm import ORMFixture
 import random
 
+
+gt = ""
+#gti = 0
+
 def test_add_contact_in_group(app, db): #orm, check_ui
+    db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+    if len (db.get_group_list()) == 0:
+        app.group.create(Group(name='test'))
+    if app.contact.count() == 0:
+        app.contact.create (Contact(Firstname="New_name", Lastname="NewLname"))
     old_groups = db.get_group_list()
-    group = random.choice(old_groups)
-    group_name=group.name
-    group_id=group.id
+    global gt
+    #global gti
+    gt = random.choice(old_groups)
+    group_name=gt.name
+    #print(gt)
+    print
     old_contacts = db.get_contact_list()
     contact = random.choice(old_contacts)
     contact_id=contact.id
+    print(contact_id)
     app.contact.add_contact_in_group(contact_id, group_name)
-    app.contact.del_contact_from_group(contact_id, group_name)
+    l = db.get_contacts_in_group(Group(id=int(gt.id)))
+    print (l)
+    #print (contact.id)
+
+
+        #app.contact.del_contact_from_group(contact_id, group_name)
 
 
     #if check_ui:
--- check_db_connection.py
+++ check_db_connection.py
@@ -5,9 +5,11 @@ from model.group import Group
 db = ORMFixture(host="127.0.0.1", name = "addressbook", user = "root", password = "")
 
 try:
-    l = db.get_contacts_not_in_group(Group(id="118"))
+    l = db.get_contacts_in_group(Group(id="118"))
     for item in l:
         print(item)
     print(len(l))
 finally:
     pass #db.destroy()
+
+
